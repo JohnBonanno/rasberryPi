129a4ea89b1ab3a2ed2ac197ed1e34873db45c67
John Bonnano! <jab0629@cmpt251.westminstercollege.edu>
2019-12-05 15:52:56 -0700

    html

--- webscript.sh
+++ webscript.sh
@@ -9,7 +9,9 @@
 
 if [ ! -e "./input.txt" ]; then 	#if no such file
 touch input.txt				#makefile
-chmod 700 input.txt			#permission
+chmod a+rw input.txt			#permission
+
+
 echo $1 >> input.txt			#write first cmdline arg to file
 
 elif [ -e "./input.txt" ]; then 	#else if file exist
